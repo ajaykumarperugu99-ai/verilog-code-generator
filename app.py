df52c01e23f666f76c14f65634281c31adf35934
ajaykumarperugu99-ai <ajaykumarperugu99@gmail.com>
2026-09-24 16:13:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ if st.button("Generate Code"):
             try:
                 # Initialize Model
                 llm = ChatGoogleGenerativeAI(
-                    model="gemini-2.5-flash",
+                    model="gemini-1.5-flash",
                     google_api_key=api_key
                 )
                 
